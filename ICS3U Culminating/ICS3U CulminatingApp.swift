8a17c6e4302c8a22083259f13cbf1a138c32767b
Yiming Fan <jfan@lcs.on.ca>
2026-06-01 14:35:08 -0400

    Initial Commit

---
+++ ICS3U Culminating/ICS3U CulminatingApp.swift
@@ -0,0 +1,17 @@
+//
+//  ICS3U CulminatingApp.swift
+//  ICS3U Culminating
+//
+//  Created by Fan Yiming on 2026-06-01.
+//
+
+import SwiftUI
+
+@main
+struct ICS3U_CulminatingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            HousesListView()
+        }
+    }
+}
